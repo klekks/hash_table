c6b7b77181ea035756c21536c9b8689cde6769b2
Илья Петров <klekks@yandex.ru>
2021-07-12 21:53:43 +0300

    Fix & definition HashTableAdd :: fixed violation of the reliability of the search HashTableFind :: definition HashTableRemove :: definition

--- hash_table/hash_table.c
+++ hash_table/hash_table.c
@@ -8,12 +8,14 @@
 #endif
 
 #include <malloc.h>
+#include <string.h> // memcmp
 
 #define MIN(A, B) A > B ? B : A
 #define MAX(A, B) A < B ? B : A
 
 #define HT_OVERFLOW(T) (((float)T->objects + T->deleted) / T->size) > T->max_occupancy
 #define HT_OVERFLOW_NEW_SIZE(T) T->objects * 2
+#define HT_OBJ(T, H) T->table[H]
 
 HT_INT is_prime(HT_INT N) {
 	if (N == 2 || N == 3 || N == 5 || N == 7) return 1;
@@ -84,14 +86,88 @@ HT_INT HashTableAdd(HashTable* table,
 	obj->data = data;
 	obj->first_hash = hash;
 	obj->second_hash = second_hash;
-	obj->key = key;
+
+	/*
+	* The key is copied to avoid changes by a pointer.
+	* 
+	*/
+	obj->key = calloc(1, key_size);
+	if (!obj->key) return 0;
+	memcpy_s(obj->key, key_size, key, key_size);
+
 	obj->key_size = key_size;
 
-	while (table->table[hash]) hash = (hash + second_hash) % table->size;
+	while (HT_OBJ(table, hash)) hash = (hash + second_hash) % table->size;
 
 	table->table[hash] = obj;
 
 	return hash;
+}
+
+void* HashTableRemove(HashTable* table,
+					void* key,
+					HT_INT key_size) {
+
+	if (!table || !key || !key_size) return NULL;
+
+	HASH first_hash = table->first_hash_function(key, key_size),
+		 second_hash = table->second_hash_function(key, key_size);
+
+	HASH hash = first_hash;
+	HTObject* obj = NULL;
+
+	while (1) {
+		if (!HT_OBJ(table, hash)) return NULL;
+		else
+			if (HT_OBJ(table, hash)->first_hash == first_hash &&
+				HT_OBJ(table, hash)->second_hash == second_hash &&
+				HT_OBJ(table, hash)->key_size == key_size) 
+				if (!memcmp(HT_OBJ(table, hash)->key, key, key_size)) {
+					obj = HT_OBJ(table, hash);
+					break;
+				}
+		hash = (hash + second_hash) % table->size;
+	}
+
+	if (!obj) return NULL;
+
+	void* data = obj->data;
+
+	free(obj->key);
+	free(obj);
+	return data;
+}
+
+void* HashTableFind(HashTable* table,
+	void* key,
+	HT_INT key_size) {
+
+	if (!table || !key || !key_size) return NULL;
+
+	HASH first_hash = table->first_hash_function(key, key_size),
+		second_hash = table->second_hash_function(key, key_size);
+
+	HASH hash = first_hash;
+	HTObject* obj = NULL;
+
+	while (1) {
+		if (!HT_OBJ(table, hash)) return NULL;
+		else
+			if (HT_OBJ(table, hash)->first_hash == first_hash &&
+				HT_OBJ(table, hash)->second_hash == second_hash &&
+				HT_OBJ(table, hash)->key_size == key_size)
+				if (!memcmp(HT_OBJ(table, hash)->key, key, key_size)) {
+					obj = HT_OBJ(table, hash);
+					break;
+				}
+		hash = (hash + second_hash) % table->size;
+	}
+
+	if (!obj) return NULL;
+
+	void* data = obj->data;
+
+	return data;
 }
 
 int main() {
